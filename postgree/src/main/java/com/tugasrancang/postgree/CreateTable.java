fc3ad46186f2372d1530b083d0fdaccc18afd2f1
Mahesa Rio Aditya <Adityario28@gmail.com>
2023-07-04 23:49:06 +0700

    update query DELETE then CREATE

--- postgree/src/main/java/com/tugasrancang/postgree/CreateTable.java
+++ postgree/src/main/java/com/tugasrancang/postgree/CreateTable.java
@@ -4,7 +4,6 @@
  */
 package com.tugasrancang.postgree;
 
-
 import java.util.Map;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +33,9 @@ public class CreateTable {
         Map<String, String> line_map = Mapping.HashMapFromTextFile(line_map_path);
         Map<String, String> table_map = Mapping.HashMapFromTextFile(table_map_path);
         try {
+            DropTable("TABLESTOCK");
+            DropTable("LINESTOCK");
+
             CreateTable(table_map, "TABLESTOCK");
             CreateTable(line_map, "LINESTOCK");
 
@@ -44,16 +46,26 @@ public class CreateTable {
         return status;
     }
 
+    public static void DropTable(String tablename) {
+        try {
+            String dropQuery = "DROP TABLE " + tablename;
+            jdbcTemplate.execute(dropQuery);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void CreateTable(Map<String, String> map, String tablename) {
         try {
-            String createQuery = "CREATE TABLE IF NOT EXISTS " + tablename + " (";
+
+            String createQuery = "CREATE TABLE " + tablename + " (";
             for (Map.Entry<String, String> entry : map.entrySet()) {
                 createQuery += entry.getKey() + " " + entry.getValue() + ", ";
                 System.out.println(entry.getKey() + ";" + entry.getValue());
             }
             if (!map.isEmpty()) {
-            createQuery = createQuery.substring(0, createQuery.length() - 2);
-        }
+                createQuery = createQuery.substring(0, createQuery.length() - 2);
+            }
             createQuery += ") ";
             jdbcTemplate.execute(createQuery);
         } catch (Exception e) {
@@ -61,3 +73,29 @@ public class CreateTable {
         }
     }
 }
+
+//
+//private static String generateCreateTableQuery(String tableName, List<String> columnData) {
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("CREATE TABLE ").append(tableName).append(" (");
+//
+//        for (int i = 0; i < columnData.size(); i++) {
+//            String[] parts = columnData.get(i).split(":");
+//            String columnName = parts[0].trim();
+//            String dataType = parts[1].trim();
+//
+//            sb.append(columnName).append(" ").append(dataType);
+//
+//            if (i < columnData.size() - 1) {
+//                sb.append(", ");
+//            }
+//        }
+//
+//        sb.append(")");
+//
+//        return sb.toString();
+//}
+//
+// fileNames.add("D:\\Tugas\\Semester9\\Pengujian Sistem\\Testing\\src\\main\\java\\map\\line_map_1.txt"); // Provide the path to your first text file here
+//        fileNames.add("D:\\Tugas\\Semester9\\Pengujian Sistem\\Testing\\src\\main\\java\\map\\table_map_1.txt"); // Provide the path to your second text file here
+//       
